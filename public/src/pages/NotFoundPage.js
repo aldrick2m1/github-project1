5f6e0e69bb2108fd35f647fd767ef280f33ae581
aldrick2m1 <114486023+aldrick2m1@users.noreply.github.com>
2022-11-04 09:38:09 +0800

    TODO APP

---
+++ public/src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1> <p>The page does not exists.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
